b2cbf038a67aa950799dcf637395de582ae7f942
nascjoao <oi@joaonasc.dev>
2021-02-23 11:15:48 -0300

    import styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 
+import styles from '../styles/pages/Home.module.css'
+
 export default function Home() {
   return (
     <div className="container">
