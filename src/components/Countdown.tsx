cfd06c78f77f381848ed0b7e845222195c058640
nascjoao <oi@joaonasc.dev>
2021-02-23 12:09:56 -0300

    structure countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,6 +2,16 @@ import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown() {
     return(
-        <div></div>
+        <div className={styles.countdownContainer}>
+            <div>
+                <span>2</span>
+                <span>5</span>
+            </div>
+            <span>:</span>
+            <div>
+                <span>0</span>
+                <span>0</span>
+            </div>
+        </div>
     )
 }
