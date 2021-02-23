9d137a93382e20eadc7d744cf18fd57291756586
nascjoao <oi@joaonasc.dev>
2021-02-23 11:23:31 -0300

    structure profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,6 +2,15 @@ import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
     return(
-        <div></div>
+        <div>
+            <img src="https://github.com/nascjoao.png" alt="João Victor Nascimento"/>
+            <div>
+                <strong>João Victor Nascimento</strong>
+                <p>
+                    <img src="" alt=""/>
+                    Level 1
+                </p>
+            </div>
+        </div>
     )
 }
