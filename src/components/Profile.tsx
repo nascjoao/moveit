bbeaf44b77bbc7a3a5b6d02b8ce868e4c082179f
nascjoao <oi@joaonasc.dev>
2021-02-23 11:31:26 -0300

    add class 'profileContainer' to div

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
     return(
-        <div>
+        <div className={styles.profileContainer}>
             <img src="https://github.com/nascjoao.png" alt="João Victor Nascimento"/>
             <div>
                 <strong>João Victor Nascimento</strong>
