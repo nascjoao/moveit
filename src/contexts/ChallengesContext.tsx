2c7a211fe485f066a1d614215f0a505a1cf45fcc
nascjoao <oi@joaonasc.dev>
2021-02-24 12:00:57 -0300

    create context api for challenges

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,3 +1,41 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
 
-export const ChallengesContext = createContext({})
+interface ChallengesProviderProps {
+    children: ReactNode
+}
+
+interface ChallengesContextData {
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+    levelUp: () => void;
+    newChallenge: () => void;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData)
+
+export function ChallengesProvider({children}: ChallengesProviderProps) {
+    const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
+
+    function levelUp() {
+        setLevel(level + 1)
+    }
+
+    function newChallenge() {
+        console.log('New Challenge!')
+    }
+
+    return (
+        <ChallengesContext.Provider value={{
+            level,
+            currentExperience,
+            challengesCompleted,
+            levelUp,
+            newChallenge
+        }}>
+            {children}
+        </ChallengesContext.Provider>
+    )
+}
