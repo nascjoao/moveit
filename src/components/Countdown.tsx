8df9cd1512ddd70a0e6623c3a00d928484c8288e
nascjoao <oi@joaonasc.dev>
2021-02-24 10:28:38 -0300

    block button when counter reach 0

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,8 +4,9 @@ import styles from '../styles/components/Countdown.module.css'
 let countdownTimeout: NodeJS.Timeout
 
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60)
+    const [time, setTime] = useState(.05 * 60)
     const [isActive, setIsActive] = useState(false)
+    const [hasFinished, setHasFinished] = useState(false)
 
     const minutes = Math.floor(time / 60)
     const seconds = time % 60
@@ -28,6 +29,9 @@ export function Countdown() {
             countdownTimeout = setTimeout(() => {
                 setTime(time - 1)
             }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true)
+            setIsActive(false)
         }
     }, [isActive, time])
 
@@ -45,10 +49,16 @@ export function Countdown() {
             </div>
         </div>
 
-        { isActive ? (
-            <button className={`${styles.countdownButton} ${styles.countdownButtonActive}`} onClick={resetCountdown}>Abandonar ciclo</button>
+        { hasFinished ? (
+            <button disabled className={styles.countdownButton}>Ciclo encerrado</button>
         ) : (
-            <button className={styles.countdownButton} onClick={startCountdown}>Iniciar um ciclo</button>
+            <>
+            { isActive ? (
+                <button className={`${styles.countdownButton} ${styles.countdownButtonActive}`} onClick={resetCountdown}>Abandonar ciclo</button>
+            ) : (
+                <button className={styles.countdownButton} onClick={startCountdown}>Iniciar um ciclo</button>
+            ) }
+            </>
         ) }
         </div>
     )
