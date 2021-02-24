f1fb8250483f9e021e8fb8fe85ea281763b742df
nascjoao <oi@joaonasc.dev>
2021-02-24 11:15:48 -0300

    finish challenge not active box

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,7 +4,7 @@ export function ChallengeBox() {
     return(
         <div className={styles.challengeBoxContainer}>
             <div className={styles.challengeNotActive}>
-                <strong>Inicie um ciclo para receber desafios</strong>
+                <strong>Inicie um ciclo<br /> para receber desafios</strong>
                 <img src="icons/level-up.svg" alt="Arrow Up"/>
                 <p>Avance de level completando os desafios.</p>
             </div>
