5cfacde9d6a58be409d72c1fe03a7efb05cd5d7c
nascjoao <oi@joaonasc.dev>
2021-02-25 11:06:28 -0300

    create countdown context

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,44 +1,19 @@
-import { useContext, useEffect, useState } from 'react'
-import { ChallengesContext } from '../contexts/ChallengesContext'
+import { useContext } from 'react'
+import { CountdownContext } from '../contexts/CountdownContext'
 import styles from '../styles/components/Countdown.module.css'
 
-let countdownTimeout: NodeJS.Timeout
-
 export function Countdown() {
-    const { newChallenge, waitForChallenge } = useContext(ChallengesContext)
-
-    const [time, setTime] = useState(.05 * 60)
-    const [isActive, setIsActive] = useState(false)
-    const [hasFinished, setHasFinished] = useState(false)
-
-    const minutes = Math.floor(time / 60)
-    const seconds = time % 60
+    const { 
+        minutes, 
+        seconds, 
+        hasFinished, 
+        isActive, 
+        resetCountdown, 
+        startCountdown 
+    } = useContext(CountdownContext)
 
     const [minuteLeft, minuteRight] = minutes.toString().padStart(2, '0').split('')
     const [secondLeft, secondRight] = seconds.toString().padStart(2, '0').split('')
-
-    function startCountdown() {
-        setIsActive(true)
-        waitForChallenge()
-    }
-
-    function resetCountdown() {
-        clearTimeout(countdownTimeout)
-        setIsActive(false)
-        setTime(25 * 60)
-    }
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1)
-            }, 1000)
-        } else if (isActive && time === 0) {
-            setHasFinished(true)
-            setIsActive(false)
-            newChallenge()
-        }
-    }, [isActive, time])
 
     return(
         <div>
