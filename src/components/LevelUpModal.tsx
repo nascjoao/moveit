45294fe87d5aa83fc6fa29ca240126483a1b69e9
nascjoao <oi@joaonasc.dev>
2021-02-26 12:17:47 -0300

    create modal structure

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -2,6 +2,14 @@ import styles from '../styles/components/LevelUpModal.module.css'
 
 export function LevelUpModal() {
     return (
-        <div></div>
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <header>5</header>
+                <strong>Parabéns</strong>
+                <p>Você alcançou um novo level!</p>
+
+                <button><img src="icons/close.svg" alt="Fechar modal"/></button>
+            </div>
+        </div>
     )
 }
